7fc7ceaaeca9cd00f531facc5b8e0cc7036f95fe
mgfun <59990670+mgfun@users.noreply.github.com>
2021-03-04 22:35:22 +0800

    Update b.js

--- test/b.js
+++ test/b.js
@@ -1,1 +1,11 @@
-$x('//*[@id="app"]/div/div[2]/div/div/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/div[2]/ul/li/img/@src').forEach(function(v){console.log('https:'+v.value.split("@")[0])})
+(function() {
+  'use strict';
+	let doc = document.evaluate('//*[@id="app"]/div/div[2]/div/div/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/div[2]/ul/li/img/@src', document.body);
+	let nodes = doc.iterateNext();
+	let alertText = '';
+	while (nodes) {
+		alertText += "https:"+nodes.textContent.split("@")[0] + "\n";
+		nodes = doc.iterateNext();
+	}
+	console.log(alertText);
+})();
